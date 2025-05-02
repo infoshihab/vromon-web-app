847790a34c5587c350f1629449ceced2d7dd4723
Shihab Uddin <infoshihab00000@gmail.com>
2025-05-02 23:46:47 +0600

    Updated design and added new files

---
+++ public/js/indexHome.js
@@ -0,0 +1,35 @@
+let taxSwtich = document.getElementById("switchCheckDefault");
+taxSwtich.addEventListener("click", () => {
+  let taxInfo = document.getElementsByClassName("tax-info");
+  for (info of taxInfo) {
+    if (info.style.display != "inline") {
+      info.style.display = "inline";
+    } else {
+      info.style.display = "none";
+    }
+  }
+});
+
+function scrollFilters(direction) {
+  document.getElementById("filters-wrapper").scrollBy({
+    left: direction,
+    behavior: "smooth",
+  });
+  setTimeout(toggleScrollButtons, 300);
+}
+
+function toggleScrollButtons() {
+  const wrapper = document.getElementById("filters-wrapper");
+  const leftBtn = document.getElementById("btn-left");
+  const rightBtn = document.getElementById("btn-right");
+
+  leftBtn.style.display = wrapper.scrollLeft > 10 ? "flex" : "none";
+  rightBtn.style.display =
+    wrapper.scrollWidth - wrapper.scrollLeft - wrapper.clientWidth > 10
+      ? "flex"
+      : "none";
+}
+
+// Run on load
+window.addEventListener("load", toggleScrollButtons);
+window.addEventListener("resize", toggleScrollButtons);
